980411b70ffdfd7fbdcd3357ed6ecfe9f8548ef8
LaoLin <hi@laolin.com>
2016-12-03 00:26:06 +0800

    用/hello/ip来试一下xhr 跨域调用jsonp

--- app/modules/index/index.module.js
+++ app/modules/index/index.module.js
@@ -4,13 +4,11 @@ angular.module('myApp.index', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/index', {
     templateUrl: 'modules/index/index.template.html',
-    controller: 'indexCtrl'
+    controller: ['$scope','$http','$log',function indexCtrl($scope,$http,$log) {
+      $http.jsonp('http://api.laolin.com/hello/ip?callback=JSON_CALLBACK&r='+Date.now()).then(function(response) {
+          $log.log(response);
+          $scope.ip = response.data.ip;
+        });
+    }]
   });
-}])
-
-.controller('indexCtrl', ['$scope',function($scope) {
-  $scope.a='123';
-  $scope.b='BASF';
-
-}])
-;
+}]);
